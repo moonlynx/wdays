566e2cefff250ff7684709da95877b3a44e3e9fd
Yuri <moonlynx@yandex.ru>
2017-11-21 22:17:12 +0300

    fix date format function

--- js/helpers.js
+++ js/helpers.js
@@ -8,10 +8,13 @@ function formatDate(date) {
     ];
   */
     var day = date.getDate(),
-        monthIndex = date.getMonth(),
+        month = date.getMonth() + 1,
         year = date.getFullYear();
+
+    if (day < 10) { day = "0" + day; }
+    if (month < 10) { month = "0" + month; }
     
-    return day + '.' + (monthIndex + 1) + '.' + year;
+    return day + '.' + month + '.' + year;
   }
   
   function getDay(date, week, isEndWeek) {
